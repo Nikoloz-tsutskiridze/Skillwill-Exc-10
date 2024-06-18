e88f6a2a757a385230c59401e54e83f41285f49b
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-06-18 23:57:08 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "../src/logo512.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="profile-image" alt="profile" />
+        <h1>nikoloz tsutskiridze</h1>
+        <p>
+          Welcome to my portfolio! Here you can find more about my work,
+          projects, and contact information.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
